b70aacc96e89ffdab0bd97bb79502c6d25a13774
Sergey <Neverstorm48@gmail.com>
2021-12-21 10:29:45 +0300

    first commit

---
+++ templates/header.php
@@ -0,0 +1,7 @@
+<header class="header">
+    <div class="header__logo"><a href="">ScreenshotHub</a></div>
+    <div class="header__buttons">
+        <button class="header__sign-in-btn btn">Sign In</button>
+        <button class="header__sign-up-btn btn">Sign Up</button>
+    </div>
+</header>
